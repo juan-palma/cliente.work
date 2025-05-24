25929e30d35f6729aee79cdfc69b9e7a6b197be1
Juan Palma <juan.a.palma.vidal@gmail.com>
2025-05-24 17:57:26 -0600

    ajsutes

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
-require 'src/MetaPreview.php';
+<?php
+	require 'src/MetaPreview.php';
 
-$preview = new MetaPreview();
-$data    = $preview->fetch($_POST[https://sca.cliente.work] ?? '');
-echo json_encode($data);
+	$preview = new MetaPreview();
+	$data    = $preview->fetch($_POST['https://sca.cliente.work'] ?? '');
+	echo json_encode($data);
+?>
