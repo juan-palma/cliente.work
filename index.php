522c74f9c600738869809441b7521506d48a1206
Juan Palma <juan.a.palma.vidal@gmail.com>
2025-05-24 18:04:13 -0600

    ajustes en cache y en la peticion en front

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 	require 'src/MetaPreview.php';
 
 	$preview = new MetaPreview();
-	$data    = $preview->fetch($_POST['https://sca.cliente.work'] ?? '');
+	//$data    = $preview->fetch($_POST['https://sca.cliente.work'] ?? '');
+	$data    = $preview->fetch('https://sca.cliente.work');
 	echo json_encode($data);
 ?>
--- src/MetaPreview.php
+++ src/MetaPreview.php
@@ -4,22 +4,29 @@ class MetaPreview
 {
     protected $cacheTtl = 86400; // 24h
 
-    public function fetch(string $url): array
+    //public function fetch(string $url): array
+    //{
+    //    $key = 'preview:' . md5($url);
+    //    // 1) Intenta caché
+    //    if ($cached = apcu_fetch($key)) {
+    //        return $cached;
+    //    }
+
+    //    // 2) Fetch y parseo
+    //    $html = $this->httpGet($url);
+    //    $data = $this->parseMeta($html, $url);
+
+    //    // 3) Guárdalo en caché
+    //    apcu_store($key, $data, $this->cacheTtl);
+
+    //    return $data;
+    //}
+
+	 public function fetch(string $url): array
     {
-        $key = 'preview:' . md5($url);
-        // 1) Intenta caché
-        if ($cached = apcu_fetch($key)) {
-            return $cached;
-        }
-
-        // 2) Fetch y parseo
+        // Eliminamos la parte del caché
         $html = $this->httpGet($url);
-        $data = $this->parseMeta($html, $url);
-
-        // 3) Guárdalo en caché
-        apcu_store($key, $data, $this->cacheTtl);
-
-        return $data;
+        return $this->parseMeta($html, $url);
     }
 
     protected function httpGet(string $url): string
