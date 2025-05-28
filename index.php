0972e54434c091d4bb94cd3d77c8397cd1318333
Juan Palma <juan.a.palma.vidal@gmail.com>
2025-05-28 14:13:15 -0600

    Agregando GTM

--- index.php
+++ index.php
@@ -41,6 +41,14 @@ foreach ($mis_sitios as $url) {
     <meta http-equiv="Content-Encoding" content="gzip" />
     <meta http-equiv="Accept-Encoding" content="gzip, deflate" />
 
+    <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-M4SVLLSH');</script>
+    <!-- End Google Tag Manager -->
+
     <title>Portafolio de Proyectos Realizados</title>
 	<meta name="description" content="Estas en nuestro portafolio de proyectos que hemos realizado para clientes, agencias y nuestros corporativos"></meta>
 
@@ -127,6 +135,12 @@ foreach ($mis_sitios as $url) {
     </style>
 </head>
 <body>
+    <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M4SVLLSH"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    
     <h1>Portafolio de Proyectos Realizados</h1>
 	<p class="intro">
 		Te encuentras en nuestro portafolio de proyectos, aquí encontraras desarrollos web hechos para clientes, agencias con quien colaboramos he incluso nuestros sitios corporativos.
